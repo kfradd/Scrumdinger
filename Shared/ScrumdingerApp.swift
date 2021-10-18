acd131ee6cbea95cafe68f04833a33290338f44c
Ken Fradd <ken@fradd.co.uk>
2021-10-18 19:40:25 +0100

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Ken Fradd on 18/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
